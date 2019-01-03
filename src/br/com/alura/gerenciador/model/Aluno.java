320fe985895a7889156e48cb4098408c45f26e17
Rafael Carvalho <crcarvalho@outlook.com>
2019-01-02 23:45:07 -0300

    primeiro commit Rafael Casa - HashCode and Equals gerado pelo eclipse na classe Aluno

--- src/br/com/alura/gerenciador/model/Aluno.java
+++ src/br/com/alura/gerenciador/model/Aluno.java
@@ -21,51 +21,51 @@ public class Aluno {
 		return numeroMatricula;
 	}
 	
+	// equals e hascode implementados pela ide Eclipse
 	@Override
 	public String toString() {
 		return "[Aluno: " + this.nome + ", Matricula: " + this.numeroMatricula + "]";
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-//		result = prime * result + numeroMatricula;
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Aluno other = (Aluno) obj;
-//		if (nome == null) {
-//			if (other.nome != null)
-//				return false;
-//		} else if (!nome.equals(other.nome))
-//			return false;
-//		if (numeroMatricula != other.numeroMatricula)
-//			return false;
-//		return true;
-//	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		Aluno outroAluno = (Aluno) obj;
-		return this.nome.equals(outroAluno.getNome());
-	}
-	
 	@Override
 	public int hashCode() {
-		
-		return this.nome.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + numeroMatricula;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (numeroMatricula != other.numeroMatricula)
+			return false;
+		return true;
 	}
 	
-	
-	
+	// equals e hascode implementados na aula do curso
+//	@Override
+//	public boolean equals(Object obj) {
+//		Aluno outroAluno = (Aluno) obj;
+//		return this.nome.equals(outroAluno.getNome());
+//	}
+//	
+//	@Override
+//	public int hashCode() {
+//		
+//		return this.nome.hashCode();
+//	}
+		
 }
